87b41be707e9e797c0bc1864a775a22af686fd2e
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-24 13:13:52 -0400

    2nd

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index')
+
+}) 
+
+app.listen(3000)
