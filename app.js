e676238483a6fa38c0bced8af368cf8c07cddbd6
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-24 14:01:33 -0400

    4th

--- app.js
+++ app.js
@@ -11,8 +11,32 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('index', {alignment: "Good",
-  										 good: true});
+  let options = {};
+  options.alignment = req.cookies.alignment;
+  options.good = req.cookies.good;
+  options.evil = req.cookies.evil;
+  res.render('index', options);
 }) 
 
 app.listen(3000)
+
+app.post('/', (req, res) => {
+  if (req.body.alignment === 'good') {
+    res.cookies.alignment = 'good';
+    res.cookies.good = true;
+    res.cookies.evil = false;
+  } else {
+    res.cookies.alignment = 'evil';
+    res.cookies.evil = true;
+    res.cookies.good = false;
+  }
+  res.redirect("/");
+})
+
+// app.post("/favorite/:id", (req, res) => {
+//   const id = req.params.id;
+//   const favorites = req.cookies.favorites || [];
+//   res.cookie("favorites", favorites.push(id));
+//   res.redirect("/");
+// });
+
